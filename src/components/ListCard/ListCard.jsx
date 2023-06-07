55fc1440bc3871e00641c3b02617d3c01d97a24c
KripakDebug <kastrula123@gmail.com>
2023-06-08 01:09:37 +0300

    active card

--- src/components/ListCard/ListCard.jsx
+++ src/components/ListCard/ListCard.jsx
@@ -26,7 +26,7 @@ export default function ListCard() {
       .get()
       .then((data) => {
         data.docs.map((doc) => {
-          setCards(doc.data());
+          return setCards(doc.data());
         });
       });
   }, [isModalOpenListChangeCard, checkedAll, firestore, idDeck, cardId]);
@@ -47,7 +47,7 @@ export default function ListCard() {
               checked={checkedAll}
               onClick={() => {
                 setCheckedAll((prevState) => !prevState);
-                // setFireStoreActiveMode(null, !checkedAll);
+                setFireStoreActiveMode(null, !checkedAll);
               }}
               className="icon-check"
             >
@@ -68,7 +68,7 @@ export default function ListCard() {
                 <Radio.Button
                   checked={item.active}
                   onClick={() => {
-                    // setFireStoreActiveMode(item.idCard);
+                    setFireStoreActiveMode(item.idCard);
                     setCardId((prevState) => prevState + 1);
                   }}
                   className="icon-check"
@@ -109,42 +109,43 @@ export default function ListCard() {
     </div>
   );
 
-  // function setFireStoreActiveMode(id = null, boolCheckAll) {
-  //   firestore
-  //     .collection("decks")
-  //     .get()
-  //     .then((data) => {
-  //       data.docs.map((doc) => {
-  //         const cards = doc.data().cards;
-  //         const updatedCards = cards.map((item) => {
-  //           if (id !== null) {
-  //             if (item.idCard === id) {
-  //               return {
-  //                 ...item,
-  //                 active: !item.active,
-  //               };
-  //             }
-  //           } else {
-  //             return {
-  //               ...item,
-  //               active: boolCheckAll,
-  //             };
-  //           }
-  //           return item;
-  //         });
-  //         doc.ref.update({ cards: updatedCards });
-  //       });
-  //     });
-  // }
-
-  function showModal() {
+  function setFireStoreActiveMode(id = null, boolCheckAll) {
     firestore
       .collection("decks")
       .get()
       .then((data) => {
         data.docs.map((doc) => {
           const cards = doc.data().cards;
-          cards.map((item) => {
+          const updatedCards = cards.map((item) => {
+            if (id !== null) {
+              if (item.idCard === id) {
+                return {
+                  ...item,
+                  active: !item.active,
+                };
+              }
+            } else {
+              return {
+                ...item,
+                active: boolCheckAll,
+              };
+            }
+            return item;
+          });
+          doc.ref.update({ cards: updatedCards });
+        });
+      });
+  }
+
+  function showModal() {
+    firestore
+      .collection("decks")
+      .where("id", "==", idDeck)
+      .get()
+      .then((data) => {
+        data.docs.forEach((doc) => {
+          const cards = doc.data().cards;
+          cards.forEach((item) => {
             if (item.active) {
               setMenuShowForRadio(true);
               setIsModalOpenListChangeCard(true);
