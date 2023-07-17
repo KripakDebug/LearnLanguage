030818b30edce3dbad5293684994dba6b0ea74e4
KripakDebug <kastrula123@gmail.com>
2023-07-18 00:17:08 +0300

    add useEffect rerender non and decks get in fetch

--- src/components/LearnCard/LearnCard.jsx
+++ src/components/LearnCard/LearnCard.jsx
@@ -4,7 +4,6 @@ import { cardsForDeckContext } from "../../App";
 import { DoubleRightOutlined } from "@ant-design/icons";
 import "./LearnCard.scss";
 import { informationWithFirebase } from "../../index";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 import CardLearn from "../CardLearn/CardLearn";
 import { useAuthState } from "react-firebase-hooks/auth";
 export default function LearnCard() {
@@ -13,41 +12,28 @@ export default function LearnCard() {
   const { auth, firestore } = useContext(informationWithFirebase);
   const [user] = useAuthState(auth);
   const currentPath = window.location.pathname.split("/");
-  const [deck] = useCollectionData(
-    firestore.collection("decks").where("userId", "==", user.uid)
-  );
   const [cards, setCards] = useState([]);
   const [lineCardsProgress, setLineCardsProgress] = useState(1);
   const [cardsLearn, setCardsLearn] = useState([]);
   useEffect(() => {
     setNavbarBool(false);
-    const filterCardsByInterval = (cards, deck) => {
-      return cards.reduce((filtered, card) => {
-        if (
-          card.estIntervalDays === null ||
-          (currentPath[2] === "practice" && card)
-        ) {
-          filtered.push({
-            card: card,
-            flashcard: deck.flashcardDeck,
-            flashcardReverse: deck.flashcardReverseDeck,
-            typing: deck.typingDeck,
-            deckName: deck.nameDeck,
-          });
-        }
-        return filtered;
-      }, []);
-    };
 
-    if (Array.isArray(deck)) {
+    const getFirestoreDecksAndPushToCards = async () => {
+      const decksFirestore = await firestore
+        .collection("decks")
+        .where("userId", "==", user.uid)
+        .get();
+
+      const decks = decksFirestore.docs.map((doc) => doc.data());
+
       let filteredCards = [];
 
       if (card === "all") {
-        deck.forEach((deck) => {
+        decks.forEach((deck) => {
           filteredCards.push(...filterCardsByInterval(deck.cards, deck));
         });
       } else {
-        const filteredDeck = deck.find((deck) => deck.id === card);
+        const filteredDeck = decks.find((deck) => deck.id === card);
         if (filteredDeck) {
           filteredCards = filterCardsByInterval(
             filteredDeck.cards,
@@ -58,8 +44,10 @@ export default function LearnCard() {
 
       const limitedCards = filteredCards.slice(0, Number(amountCard));
       setCards(limitedCards);
-    }
-  }, [amountCard, card, deck, setNavbarBool]);
+    };
+
+    getFirestoreDecksAndPushToCards();
+  }, []);
   return (
     <div className="container">
       <div className="learn">
@@ -94,4 +82,22 @@ export default function LearnCard() {
       </div>
     </div>
   );
+
+  function filterCardsByInterval(cards, deck) {
+    return cards.reduce((filtered, card) => {
+      if (
+        card.estIntervalDays === null ||
+        (currentPath[2] === "practice" && card)
+      ) {
+        filtered.push({
+          card: card,
+          flashcard: deck.flashcardDeck,
+          flashcardReverse: deck.flashcardReverseDeck,
+          typing: deck.typingDeck,
+          deckName: deck.nameDeck,
+        });
+      }
+      return filtered;
+    }, []);
+  }
 }
